40e879bbb8c40aa8b2d04a26b87820019b80cb9e
Mathyas P <connectwithmathyas01@gmail.com>
2026-05-05 13:09:41 +0530

    final fix working home route

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ContactMessageCreateView,home
-urlpatterns = [
-    path('contact/', ContactMessageCreateView.as_view(), name='contact_create'),
+from .views import ContactMessageCreateView, home
 
+urlpatterns = [
+    path('', home, name='api_home'),
+    path('contact/', ContactMessageCreateView.as_view(), name='contact_create'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,23 +1,24 @@
 from rest_framework import generics
-from django.core.mail import send_mail
+# from django.core.mail import send_mail
 from django.conf import settings
 from .models import ContactMessage
 from .serializers import ContactMessageSerializer
 from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("API is running")
+
 class ContactMessageCreateView(generics.CreateAPIView):
     queryset = ContactMessage.objects.all()
     serializer_class = ContactMessageSerializer
 
-
     def perform_create(self, serializer):
         instance = serializer.save()
-        send_mail(
-            subject=f"New Message from Portfolio - {instance.name}",
-            message=f"Name: {instance.name}\nEmail: {instance.email}\nMessage: {instance.message}",
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[settings.EMAIL_HOST_USER],
-            fail_silently=False,
-        )
-
-    
+        # Temporarily disabling email sending to prevent 500 errors on Render
+        # send_mail(
+        #     subject=f"New Message from Portfolio - {instance.name}",
+        #     message=f"Name: {instance.name}\nEmail: {instance.email}\nMessage: {instance.message}",
+        #     from_email=settings.EMAIL_HOST_USER,
+        #     recipient_list=[settings.EMAIL_HOST_USER],
+        #     fail_silently=False,
+        # )
